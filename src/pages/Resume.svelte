e2dea7816e744f03c27bd8d982a1eeefc08bf737
Yonah Aviv <68514914+y330@users.noreply.github.com>
2023-11-14 18:35:12 -0500

    Update Resume.svelte

--- src/pages/Resume.svelte
+++ src/pages/Resume.svelte
@@ -10,7 +10,7 @@
 <section>
 	<Subhead><em>Drag corners to resize</em></Subhead>
 	<iframe
-		src="<iframe src="https://onedrive.live.com/embed?resid=D1BCEE1A2FD48DEE%213731&authkey=!AHaM9PpTpwri-Hs&em=2"
+		src="https://onedrive.live.com/embed?resid=D1BCEE1A2FD48DEE%213731&authkey=!AHaM9PpTpwri-Hs&em=2"
 		height="800"
 		frameborder="0"
 		width="100%"
